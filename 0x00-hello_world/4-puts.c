2914ffd7f8aed78dda63d0bf9b5cf4812355cd16
henok birhanu <enoch3043@gmail.com>
2022-12-08 13:13:23 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
-#includ <stdio.h>
-#includ <string.h>
+#include <stdio.h>
+#include <string.h>
 /**
  * main-print a predefined string
  *
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-  puts("\" Programming is like building a multilingual puzzle');
+  puts("\" Programming is like building a multilingual puzzle");
   return (0);
 
 }
