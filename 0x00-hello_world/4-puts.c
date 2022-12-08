f5dbd4817ada4e90a05010c2898d24eaaf6483e3
henok birhanu <enoch3043@gmail.com>
2022-12-08 14:26:51 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
-#include <string.h>
-/**
- * main-print a predefined string
- *
- * return: 0 for sucess
+/*
+ * File: 4-puts.c
+ * Auth: Brennan D Baraban <375@holbertonschool.com>
  */
 
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-puts("\" Programming is like building a multilingual puzzle');
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
