74308eb5ee59a25a1d5ba0b3f0092a2fae4828a0
henok birhanu <enoch3043@gmail.com>
2022-12-15 09:16:27 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,14 @@
  */
 int main(void)
 {
-	unsigned int i;
-	char s[10] = "_putchar\n";
-
-	for (i = 0; i < sizeof(s); i++)
-	{
-		_putchar(s[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
