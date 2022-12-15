d7f508ca99cf9d9eddfe7ac469a579b237c89995
henok birhanu <enoch3043@gmail.com>
2022-12-15 09:55:52 -0800

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks if character is a letter
+ * @c: ascii code of character to check
+ *
+ * Return: 1 if character is a letter, 0 if not
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+
+	return (0);
+}
