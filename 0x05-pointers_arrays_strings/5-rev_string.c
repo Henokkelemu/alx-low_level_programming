c61becfce85764d6e2b4be203bab3cea7cbb7588
henok birhanu <enoch3043@gmail.com>
2022-12-20 22:46:01 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * rev_string - Reverses a string in place
+ * @s: pointer to the first character in the string
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	char *str = s;
+
+	for (; *str; str++)
+		;
+	str--;
+
+	for (; s < str; s++, str--)
+	{
+		tmp = *s;
+		*s = *str;
+		*str = tmp;
+	}
+}
