233a264e0a99f1bfce8b6ed6a8478fe45f5d4101
henok birhanu <enoch3043@gmail.com>
2022-12-21 03:18:48 -0800

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strlen - returns the length of a string
- * @s: pointer to first character of string
+ * _strncpy - Copies at most an inputted number
+ *            of bytes from string src into dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copied from src.
  *
- * Return: length of string
+ * Return: A pointer to the resulting string dest.
  */
-int _strlen(char *s)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int index = 0, src_len = 0;
 
-	for (; *s != '\0'; s++)
-		i++;
+	while (src[index++])
+		src_len++;
 
-	return (i);
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
 }
-
