790f8a3e6901ab68a6fbf3c56a14b5d6c5b9c3d6
henok birhanu <enoch3043@gmail.com>
2022-12-20 22:43:12 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string backwards
+ * @s: pointer to the first character in the string
+ */
+void print_rev(char *s)
+{
+	int len, i;
+
+	len = 0;
+
+	for (; *s; s++)
+		len++;
+
+	s--;
+
+	for (i = 0; i < len; i++)
+		_putchar(*(s--));
+
+	_putchar('\n');
+}
